237a315c03a59a99619c50dbdee6fbf83fa127b6
receiptscdsolutions-hub <receipts.cdsolutions@gmail.com>
2026-01-10 21:22:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const upload = multer({
   storage: multer.memoryStorage(),
